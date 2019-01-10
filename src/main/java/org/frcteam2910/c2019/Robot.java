39b6cdcbdccc0170c1a67ab32e0ef33079b0831b
Kaboomboom3 <Jacob@Bublitz.com>
2019-01-09 20:21:25 -0800

    Adds base robot code (#5)

---
+++ src/main/java/org/frcteam2910/c2019/Robot.java
@@ -0,0 +1,25 @@
+package org.frcteam2910.c2019;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import org.frcteam2910.common.robot.subsystems.SubsystemManager;
+
+public class Robot extends TimedRobot {
+    private static final double UPDATE_DT = 5e-3; // 5 ms
+
+    private final SubsystemManager subsystemManager = new SubsystemManager();
+
+    @Override
+    public void teleopInit() {
+        subsystemManager.enableKinematicLoop(UPDATE_DT);
+    }
+
+    @Override
+    public void autonomousInit() {
+        subsystemManager.enableKinematicLoop(UPDATE_DT);
+    }
+
+    @Override
+    public void disabledInit() {
+        subsystemManager.disableKinematicLoop();
+    }
+}
